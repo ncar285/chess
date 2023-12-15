d97b187974f5aa1c955ee2ce6de15b0a53902005
ncar285 <carlier.nicholas@gmail.com>
2023-12-15 16:38:31 +1300

    add squares to chessboard

--- assets/main.js
+++ assets/main.js
@@ -2,4 +2,28 @@ document.addEventListener("DOMContentLoaded", function() {
     if (document.body.classList.contains('is-preload')) {
         document.body.classList.remove('is-preload');
     }
+
+    setupChessBoard();
 });
+
+function setupChessBoard(){
+    const board = document.querySelector('.chess-board');
+    let color = "brown";
+    for (let a = 0 ; a  < 8 ; a++ ){
+        const row = document.createElement('div');
+        const rank = a + 1;
+        row.id = `rank-${rank}`;
+        row.className = `board-row ${a+1}`;
+        board.prepend(row);
+        for (let b = 0 ; b  < 8 ; b++ ){
+            const square = document.createElement('div');
+            const charCode = 'a'.charCodeAt(0) + b;
+            const file = String.fromCharCode(charCode)
+            square.id = `${rank}-${file}`;
+            square.className = `board-square ${color}`;
+            row.appendChild(square);
+            color = (color === "brown") ? "white" : "brown";
+        }
+        color = (color === "brown") ? "white" : "brown";
+    }
+}
