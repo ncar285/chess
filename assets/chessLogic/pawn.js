48a1644175baefc606a9f8cfd15ccfa2c3578182
ncar285 <carlier.nicholas@gmail.com>
2023-12-15 17:32:54 +1300

    add pawn behaviour

--- assets/chessLogic/pawn.js
+++ assets/chessLogic/pawn.js
@@ -2,10 +2,27 @@ const Piece = require("./piece.js");
 const inherit = require("./inherit.js");
 
 function Pawn(square){
-    Piece.call(this, color);
-    this.square;
+    Piece.call(this, color, square);
 }
 
 inherit(Piece, Pawn);
+
+Pawn.prototype.getMoves = function(){
+    const [rank, file] = this.square;
+    const color = this.getColor;
+    const options = [];
+    if (color === "white"){
+        options.push([rank + 1, file]);
+        if (rank === 2){
+            options.push([rank + 2, file]);
+        }
+    } else {
+        options.push([rank - 1, file]);
+        if (rank === 7){
+            options.push([rank - 2, file]);
+        }
+    }
+    return options;
+}
 
 module.exports = Pawn;
--- assets/chessLogic/piece.js
+++ assets/chessLogic/piece.js
@@ -1,5 +1,18 @@
-function Piece(color){
+function Piece(color, square){
     this.color = color;
+    this.square = square;
+}
+
+Piece.prototype.getColor = function(){
+    return this.color;
+}
+
+Piece.prototype.getSquare = function(){
+    return this.square;
+}
+
+Piece.prototype.setSquare = function(pos){
+    this.square = pos;
 }
 
 module.exports = Piece;
