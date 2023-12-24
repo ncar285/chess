d5f30b6596832c1707e4dbbd9d816265b8ff2492
ncar285 <carlier.nicholas@gmail.com>
2023-12-24 17:27:16 +1300

    add corrected selection styling

--- src/components/ChessSquare/ChessSquare.jsx
+++ src/components/ChessSquare/ChessSquare.jsx
@@ -1,9 +1,8 @@
 // import { clickMove } from "../../Utils/pieceMovement";
 import { useSelector } from "react-redux";
 import ChessPiece from "../ChessPiece/ChessPiece";
-import "./ChessSquare.css"
 import { getSelected } from "../../store/uiReducer";
-import { useEffect } from "react";
+import "./ChessSquare.css"
 
 function ChessSquare( {squareParams} ) {
 
@@ -11,24 +10,12 @@ function ChessSquare( {squareParams} ) {
     const [file, rank] = id.split("");
 
     const selectedSquare = useSelector(getSelected);
-    let fullClassName = className;
-    useEffect(()=>{
-        if (selectedSquare === id) fullClassName = `${className} selected`;
-        console.log("fullClassName", fullClassName)
-    }, [selectedSquare])
-
-    let source = "";
-
-    if (pieceObj){
-        const name = `${pieceObj.getColor() === "white" ? "w_" : "b_"}${pieceObj.getType()}`;
-        source = `/src/pieces/${name}.png`;
-    }
     
     return (
-        <div id={id} className={fullClassName}>
+        <div id={id} className={`${className} ${(selectedSquare === id) ? 'selected' : ''}`}>
             {rankLabel && <div className="rank square-label">{rank}</div>}
             {fileLabel && <div className="file square-label">{file.toLowerCase()}</div>}
-            {pieceObj && <ChessPiece pieceObj={pieceObj} source={source}/>}
+            {pieceObj && <ChessPiece pieceObj={pieceObj}/>}
         </div>
     )
 
